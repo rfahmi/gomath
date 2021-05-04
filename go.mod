95baa7608388ca7a95c7858f618019e4a5fc47d6
rfahmi <rfahmi@outlook.com>
2021-05-04 10:52:58 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rfahmi/gomath
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,10 @@
+package gomath
+
+func Sum(nums ...int64) int64 {
+	var res int64
+	for _, v := range nums {
+		res += res + v
+	}
+
+	return res
+}
